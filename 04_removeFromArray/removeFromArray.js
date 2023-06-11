0eeee1f1558971a86109beecc09b7c367a21b6af
Byron <133577261+byronjarebb96@users.noreply.github.com>
2023-06-11 08:59:25 +0400

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,21 @@
-const removeFromArray = function(array, removeThis) {
+const removeFromArray = function(array, ...removeThese) {
+    for (removalItem of removeThese)
+    {
+        if (array.includes(removalItem)==1)
+        {
+            removalIndex = array.indexOf(removalItem);
+            array.splice(removalIndex,1);
+        }
+    }
+    return array;
+    /*
     if (removeThis > 1)
     {
         removalIndex = array.indexOf(removeThis);
         array.splice(removalIndex, 1);
     }
     return array;
+    */
 };
 
 // Do not edit below this line
