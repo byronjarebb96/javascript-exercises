5dea216126a893745817873619bbbfb6b9ec9efb
Byron <133577261+byronjarebb96@users.noreply.github.com>
2023-07-14 12:52:55 +0400

    Add first part of palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const reversedString = string.split('').reverse().join('');
+    if (string === reversedString) return true;
+    else return false;
 };
 
 // Do not edit below this line
